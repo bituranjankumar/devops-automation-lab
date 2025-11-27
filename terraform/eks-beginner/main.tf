09b8e4ebdb79c7a276bdf06255c4a48dce799c5a
B Ranjan Kumar <154856585+bituranjankumar@users.noreply.github.com>
2025-11-27 23:41:47 +0800

    Add main.tf for EKS beginner project

---
+++ terraform/eks-beginner/main.tf
@@ -0,0 +1,102 @@
+############################################
+# main.tf
+# Core Terraform resources for AWS EKS
+############################################
+
+# Create an IAM role for the EKS cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.tags
+}
+
+# Attach policies for EKS cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create an EKS cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  tags = var.tags
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+# Create IAM role for EKS nodes
+resource "aws_iam_role" "node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = var.tags
+}
+
+# Attach policies for worker nodes
+resource "aws_iam_role_policy_attachment" "worker_policies" {
+  count      = 3
+  role       = aws_iam_role.node_role.name
+  policy_arn = element([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ], count.index)
+}
+
+# Create managed node group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.cluster_name}-nodes"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.desired_capacity + 1
+    min_size     = 1
+  }
+
+  instance_types = [var.node_instance_type]
+  tags           = var.tags
+
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
+
+# Get the default VPC and subnets
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
