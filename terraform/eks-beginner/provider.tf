a78fcc66dfbdebe6d52ba70259a012cacd0610c6
B Ranjan Kumar <154856585+bituranjankumar@users.noreply.github.com>
2025-11-27 23:38:03 +0800

    Add provider.tf for EKS beginner project

---
+++ terraform/eks-beginner/provider.tf
@@ -0,0 +1,18 @@
+############################################
+# provider.tf
+# Configures Terraform and AWS provider
+############################################
+
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
